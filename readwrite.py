d000fead097516241cc6f176479c267e11b0c7d8
Joshua Mensah <joshmensah18@gmail.com>
2023-09-08 09:22:26 +0000

    Fix "Log for Current Date"

--- readwrite.py
+++ readwrite.py
@@ -23,19 +23,22 @@ def readfile(date=get_date()):
 
     logs = []
 
-    with open(f"./logs/{date}.txt", 'r') as f:
-        file = [x.strip() for x in f.readlines()]
-    
-    if len(file) != 0:
-        for f in file:
-            f = f.split("::")
-            logs.append(Log(f[0], f[1]))
-    else:
-        logs = [f":: No Activities for {date} "]
+    try:
+        with open(f"./logs/{date}.txt", 'r') as f:
+            file = [x.strip() for x in f.readlines()]
+            
+            if len(file) != 0:
+                for f in file:
+                    f = f.split("::")
+                    logs.append(Log(f[0], f[1]))
+                else:
+                    logs = [f":: No Activities for {date} "]
 
+    except FileNotFoundError:
+        logs = [f":: No Activities for {date} "]
     
+   
     print("Logs read")
-    print(logs)
     logs.reverse()
     return logs
 
