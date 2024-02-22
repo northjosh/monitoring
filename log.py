eef8673b3ec6fdf2ad602b9ffffa42ae6492af1b
Joshua Mensah <joshmensah18@gmail.com>
2024-02-22 22:12:32 +0000

    Test linter

--- log.py
+++ log.py
@@ -1,7 +1,16 @@
+"""
+Log Class
+
+"""
 class Log:
+    """
+    Python class to store logs as log objects
+
+    """
     def __init__(self, time, message):
         self.time = time
         self.message = message
-    
+
     def __str__(self) -> str:
         return f"{self.time}: {self.message}"
+    
